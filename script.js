826ffa67e855412a132a278e123d295ed602d94b
Aman garg <amankmrgarg@gmail.com>
2024-07-16 17:58:49 +0530

    Added the cart successfully

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// For closing&opning navbar in mobile screeen
+
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
-const like = document.querySelectorAll(".like");
 
 // console.log(like);
 if (bar) {
@@ -16,6 +17,9 @@ if (close) {
     })
 }
 
+// For adding the like button
+
+const like = document.querySelectorAll(".like");
 
 like.forEach(element => {
     element.addEventListener('click', () => {
@@ -35,9 +39,10 @@ var image = document.querySelector(".pro-image")
 // console.log(cartBtn);
 
 var cartDetails = [];
+// var userdata = [];
 
 let sendData = () => {
-    // event.preventDefault()
+
     cartDetails.push({
         Name: namep.innerText,
         Price: price.innerText,
@@ -46,32 +51,28 @@ let sendData = () => {
         Image: image.getAttribute('src')
     })
     var userdata = JSON.stringify(cartDetails);
+    // console.log(userdata);
     localStorage.setItem("userData", userdata)
 
-    alert("You successfully added the product to cart")
+    alert("You successfully added the product to the cart 🛒")
 }
-
-
-
 
 // console.log(cartDetails);
 
-function buy(){
-    alert("You buy the item")
+function buy() {
+    alert("You successfully buy an item 🛍️")
 }
+
+var getdata = [];
 function cart() {
-    var Img = document.querySelector(".cimg");
-    var Name = document.querySelector(".cname");
-    var Price = document.querySelector(".cprice");
-    var Qtt = document.querySelector(".cqt");
-    var Csize = document.querySelector(".csize ")
-    var Buybtn = document.querySelector(".ccart ");
-    let Listitem = document.querySelector(".listitem")
-    if (localStorage.getItem("userData") !== null) {
-        var getdata = JSON.parse(localStorage.getItem("userData"))
-        
-    }
-    // console.log(getdata);
+
+    // For static product added to the cart
+    // var Img = document.querySelector(".cimg");
+    // var Name = document.querySelector(".cname");
+    // var Price = document.querySelector(".cprice");
+    // var Qtt = document.querySelector(".cqt");
+    // var Csize = document.querySelector(".csize ")
+    // var Buybtn = document.querySelector(".ccart ");
     // const arr = [{
     //     Image:"img/products/f1.jpg",
     //     Name:"Men's Fashion T-Shirt",
@@ -79,12 +80,25 @@ function cart() {
     //     Quantity:"1",
     //     Size: "XXL",
     // }]
-    
-    // event.preventDefault()
-    // let data = 0;
-    getdata.forEach((e)=>{
-        let itemlist =  
-            `<div class="single-pro-image pg">
+    // Img.setAttribute("src",getdata[0].Image)
+    //     Name.innerHTML = getdata[0].Name;
+    //     Price.innerHTML = getdata[0].Price;
+    //     Qtt.setAttribute("value",getdata[0].Quantity)
+    //    let op =  document.createElement("option")
+    //    op.innerHTML = getdata[0].Size;
+    //    Csize.append(op)
+
+    let Listitem = document.querySelector(".listitem")
+    if (localStorage.getItem("userData") !== null) {
+        getdata = JSON.parse(localStorage.getItem("userData"))
+        cartDetails = getdata
+    }
+    // console.log(getdata);
+
+
+    getdata.forEach((e, index) => {
+        let itemlist =
+            `<div class="single-pro-image pg" data-id="${index}">
                     <img src="${e.Image}" width="100%" id="mainimg" class="cimg">
                 <div class="single-pro-details cs">
                     <h6>Home / T-Shirt</h6>
@@ -94,19 +108,24 @@ function cart() {
                     <option>${e.Size}</option>
                     </select>
                     <input type="number" value="${e.Quantity}" class="cqt">
-                    <button class="normal ccart" onclick ="buy()">Buy Now</button>
-                </div>
+                    </div>
+                    <button class="ccart" onclick ="buy()">Buy Now</button>
+                    <button class="remove-btn" onclick ="rvBtn(${index})" >Remove</button>
             </div>
             `
         Listitem.innerHTML += itemlist
     })
-    // Img.setAttribute("src",getdata[0].Image)
-//     Name.innerHTML = getdata[0].Name;
-//     Price.innerHTML = getdata[0].Price;
-//     Qtt.setAttribute("value",getdata[0].Quantity)
-//    let op =  document.createElement("option")
-//    op.innerHTML = getdata[0].Size;
-//    Csize.append(op)
 }
 cart();
 
+// For removing the item int the cart
+
+function rvBtn(index) {
+    if (confirm("Are you sure want to delete? ⚠️ ")) {
+        getdata.splice(index, 1)
+        localStorage.setItem("userData", JSON.stringify(cartDetails))
+        cartDetails = getdata
+        // console.log(cartDetails)
+        // cart()
+    }
+}
